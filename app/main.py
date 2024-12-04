5cc16561d26ac3f2bf0cb32257a85f2da7d9e5f1
Olena Bukhova <lena.bukhova2005@gmail.com>
2024-12-04 12:42:05 +0100

    add get() and list comprehension

--- app/main.py
+++ app/main.py
@@ -8,15 +8,14 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    people_list = []
-    for person_data in people:
-        people_list.append(Person(person_data["name"], person_data["age"]))
+    people_list = [Person(person_data["name"], person_data["age"])
+                   for person_data in people]
 
     for person_data in people:
-        person = Person.people[person_data["name"]]
+        person = Person.people.get(person_data["name"])
         if "wife" in person_data and person_data["wife"]:
-            person.wife = Person.people[person_data["wife"]]
+            person.wife = Person.people.get(person_data["wife"])
         elif "husband" in person_data and person_data["husband"]:
-            person.husband = Person.people[person_data["husband"]]
+            person.husband = Person.people.get(person_data["husband"])
 
     return people_list
